3be44053b8fb2ec468b68ae3a91d8eea6520be63
kuzmitchoww <ak.kuzmichoww@gmail.com>
2024-02-03 09:16:58 +0100

    Text to morse code main.py changed (add some comments)

--- Text to Morse Code Converter/main.py
+++ Text to Morse Code Converter/main.py
@@ -27,9 +27,12 @@ MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
 def text_to_morse_converter():
     # Get input from user
     text = input("Enter text to convert into morse code here: \n")
-    # Convert input into a list of UpperCase letters
+    # Convert input into a list of UpperCase letters (using list comprehension)
     text_to_list = [i.upper() for i in list(text)]
+    # Get morse equivalent for every symbol in text using item in text_to_list as a key for a MORSE_CODE_DICT
+    # (using list comprehension)
     text_list_to_morse_list = [MORSE_CODE_DICT[i] + ' ' for i in text_to_list]
+    # Convert a list of morse symbols into result string to return
     converted_text = "".join(text_list_to_morse_list)
 
     return f"Here is your text converted into morse code:\n{converted_text}"
